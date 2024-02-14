5a293048330afbba411d8d42cbe47c5ba7798298
mrzamin <142754418+mrzamin@users.noreply.github.com>
2024-02-14 13:27:35 -0500

    Create gameboard, gameController, and cell fcns with module design pattern

--- tictactoe.js
+++ tictactoe.js
@@ -2,31 +2,91 @@
 I am storing the gameboard as an arrat inside of a gameboard object.
 */
 
-function Gameboard() {
-  const coordinates = 9;
+function gameboard() {
+  const rows = 3;
+  const columns = 3;
   const board = [];
 
-  for (let i = 0; i < coordinates; i++) {
+  for (let i = 0; i < rows; i++) {
     board[i] = [];
-    /* Come back here to add push method */
+    for (let j = 0; j < columns; j++) {
+      board[i].push(cell());
+    }
   }
 
   //Method of getting our entire gameboard that our UI will need to render it.
   const getBoard = () => board;
 
-  const dropToken = (chosenCoordinate, player) => {
-    const availableCoordinate = board[chosenCoordinate].getValue() === 0;
+  const dropToken = (row, column, player) => {
+    const availableCoordinate = board[row][column].getValue() === 0;
 
     if (!availableCoordinate) return;
-    board[chosenCoordinate].addToken(player);
+    board[row][column].addToken(player);
   };
 
   const printBoard = () => {
-    const boardWithTokenValues = board.map((coordinate) =>
-      coordinate.getValue()
+    const boardWithTokenValues = board.map((row) =>
+      row.map((column) => column.getValue())
     );
     console.log(boardWithTokenValues);
   };
 
   return { getBoard, dropToken, printBoard };
 }
+
+function cell() {
+  let value = 0;
+
+  const addToken = (playerToken) => {
+    value = playerToken;
+  };
+
+  const getValue = () => value;
+
+  return {
+    addToken,
+    getValue,
+  };
+}
+
+function gameController(
+  playerOneName = "Player One",
+  playerTwoName = "Player Two"
+) {
+  const board = gameboard();
+
+  const players = [
+    { name: playerOneName, token: "X" },
+
+    { name: playerTwoName, token: "O" },
+  ];
+
+  let activePlayer = players[0];
+
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
+
+  const getActivePlayer = () => activePlayer;
+
+  const printNewRound = () => {
+    board.printBoard();
+    console.log(`${getActivePlayer().name}'s turn`);
+  };
+
+  const playRound = (chosenCoordinate) => {
+    board.dropToken(chosenCoordinate, getActivePlayer().token);
+    console.log(`Dropping ${getActivePlayer().name}'s token...`);
+    switchPlayerTurn();
+    printNewRound();
+  };
+
+  printNewRound();
+
+  return {
+    playRound,
+    getActivePlayer,
+  };
+}
+
+const game = gameController();
