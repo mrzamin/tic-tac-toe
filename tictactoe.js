16c808f6fd29aae2519b6e58278f7157b7ba5135
mrzamin <142754418+mrzamin@users.noreply.github.com>
2024-02-14 15:17:04 -0500

    add screenController() fcn using module design pattern

--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,5 @@
 /*The Gameboard represents the state of the board.
-I am storing the gameboard as an arrat inside of a gameboard object.
+I am storing the gameboard as an array inside of a gameboard object.
 */
 
 function gameboard() {
@@ -24,14 +24,14 @@ function gameboard() {
     board[row][column].addToken(player);
   };
 
-  const printBoard = () => {
-    const boardWithTokenValues = board.map((row) =>
-      row.map((column) => column.getValue())
-    );
-    console.log(boardWithTokenValues);
-  };
+  // const printBoard = () => {
+  //   const boardWithTokenValues = board.map((row) =>
+  //     row.map((column) => column.getValue())
+  //   );
+  //   console.log(boardWithTokenValues);
+  // };
 
-  return { getBoard, dropToken, printBoard };
+  return { getBoard, dropToken };
 }
 
 function cell() {
@@ -69,24 +69,65 @@ function gameController(
 
   const getActivePlayer = () => activePlayer;
 
-  const printNewRound = () => {
-    board.printBoard();
-    console.log(`${getActivePlayer().name}'s turn`);
-  };
+  // const printNewRound = () => {
+  //   board.printBoard();
+  //   console.log(`${getActivePlayer().name}'s turn`);
+  // };
 
-  const playRound = (chosenCoordinate) => {
-    board.dropToken(chosenCoordinate, getActivePlayer().token);
+  const playRound = (row, column) => {
+    board.dropToken(row, column, getActivePlayer().token);
     console.log(`Dropping ${getActivePlayer().name}'s token...`);
     switchPlayerTurn();
-    printNewRound();
+    // printNewRound();
   };
 
-  printNewRound();
+  // printNewRound();
 
   return {
     playRound,
     getActivePlayer,
+    getBoard: board.getBoard,
   };
 }
 
-const game = gameController();
+function screenController() {
+  const game = gameController();
+  const boardDiv = document.querySelector(".board");
+  const playerTurnDiv = document.querySelector(".turn");
+
+  const updateScreen = () => {
+    boardDiv.textContent = "";
+
+    const board = game.getBoard();
+    const activePlayer = game.getActivePlayer();
+
+    playerTurnDiv.textContent = `${activePlayer.name}'s turn...`;
+
+    board.forEach((row, rowIndex) => {
+      row.forEach((column, columnIndex) => {
+        const squareBtn = document.createElement("button");
+        squareBtn.classList.add("square");
+        squareBtn.dataset.column = columnIndex;
+        squareBtn.dataset.row = rowIndex;
+        squareBtn.textContent = column.getValue();
+        boardDiv.appendChild(squareBtn);
+      });
+    });
+  };
+
+  function boardClickHandler(e) {
+    const selectedColumn = e.target.dataset.column;
+    const selectedRow = e.target.dataset.row;
+
+    if (!selectedColumn) return;
+    if (!selectedRow) return;
+
+    game.playRound(selectedRow, selectedColumn);
+    updateScreen();
+  }
+  boardDiv.addEventListener("click", boardClickHandler);
+
+  updateScreen();
+}
+
+screenController();
