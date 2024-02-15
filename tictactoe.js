71818b4617356a5e79368745fc60ee0717ebe008
mrzamin <142754418+mrzamin@users.noreply.github.com>
2024-02-15 14:51:15 -0500

    Add win logic

--- tictactoe.js
+++ tictactoe.js
@@ -24,13 +24,6 @@ function gameboard() {
     board[row][column].addToken(player);
   };
 
-  // const printBoard = () => {
-  //   const boardWithTokenValues = board.map((row) =>
-  //     row.map((column) => column.getValue())
-  //   );
-  //   console.log(boardWithTokenValues);
-  // };
-
   return { getBoard, dropToken };
 }
 
@@ -41,11 +34,16 @@ function cell() {
     value = playerToken;
   };
 
+  const clearCellValue = () => {
+    value = 0;
+  };
+
   const getValue = () => value;
 
   return {
     addToken,
     getValue,
+    clearCellValue,
   };
 }
 
@@ -55,6 +53,8 @@ function gameController(
 ) {
   const board = gameboard();
 
+  const gameArray = board.getBoard();
+
   const players = [
     { name: playerOneName, token: "X" },
 
@@ -63,29 +63,83 @@ function gameController(
 
   let activePlayer = players[0];
 
+  let gameOver = false;
+
+  const getGameStatus = () => gameOver;
+
+  const resetGame = () => {
+    activePlayer = players[0];
+    gameOver = false;
+  };
+
   const switchPlayerTurn = () => {
     activePlayer = activePlayer === players[0] ? players[1] : players[0];
   };
 
   const getActivePlayer = () => activePlayer;
 
-  // const printNewRound = () => {
-  //   board.printBoard();
-  //   console.log(`${getActivePlayer().name}'s turn`);
-  // };
+  const checkWinner = () => {
+    const [
+      //use array destructuring to assign variable names to each cell
+      [cell0, cell1, cell2],
+      [cell3, cell4, cell5],
+      [cell6, cell7, cell8],
+    ] = gameArray;
+
+    console.log(cell0);
+    console.log(cell0.getValue());
+    console.log(cell1.getValue());
+
+    console.log(cell2.getValue());
+
+    const winCombinations = [
+      [cell0, cell1, cell2], //rows
+      [cell3, cell4, cell5],
+      [cell6, cell7, cell8],
+      [cell0, cell3, cell6], //columns
+      [cell1, cell4, cell7],
+      [cell2, cell5, cell8],
+      [cell0, cell4, cell8], //diagnals
+      [cell2, cell4, cell6],
+    ];
+
+    for (let i = 0; i < winCombinations.length; i++) {
+      const winCombination = winCombinations[i];
+      const cellA = winCombination[0].getValue();
+      const cellB = winCombination[1].getValue();
+      const cellC = winCombination[2].getValue();
+
+      if (cellA !== 0 && cellB !== 0 && cellC !== 0) {
+        if (cellA === cellB && cellB === cellC) {
+          gameOver = true;
+
+          // gameArray.forEach((row) => {
+          //   row.forEach((column) => {
+          //     column.clearCellValue();
+          //   });
+          // });
+          break;
+        }
+      }
+    }
+  };
 
   const playRound = (row, column) => {
     board.dropToken(row, column, getActivePlayer().token);
-    console.log(`Dropping ${getActivePlayer().name}'s token...`);
-    switchPlayerTurn();
-    // printNewRound();
-  };
+    // console.log(`Dropping ${getActivePlayer().name}'s token...`);
+    checkWinner();
 
-  // printNewRound();
+    if (gameOver) {
+      return;
+    } else {
+      switchPlayerTurn();
+    }
+  };
 
   return {
     playRound,
     getActivePlayer,
+    getGameStatus,
     getBoard: board.getBoard,
   };
 }
@@ -100,8 +154,13 @@ function screenController() {
 
     const board = game.getBoard();
     const activePlayer = game.getActivePlayer();
+    const gameStatus = game.getGameStatus();
 
-    playerTurnDiv.textContent = `${activePlayer.name}'s turn...`;
+    if (gameStatus) {
+      playerTurnDiv.textContent = `${activePlayer.name} won!!!`;
+    } else {
+      playerTurnDiv.textContent = `${activePlayer.name}'s turn...`;
+    }
 
     board.forEach((row, rowIndex) => {
       row.forEach((column, columnIndex) => {
@@ -110,6 +169,7 @@ function screenController() {
         squareBtn.dataset.column = columnIndex;
         squareBtn.dataset.row = rowIndex;
         squareBtn.textContent = column.getValue();
+        squareBtn.addEventListener("click", boardClickHandler, { once: true });
         boardDiv.appendChild(squareBtn);
       });
     });
@@ -118,14 +178,18 @@ function screenController() {
   function boardClickHandler(e) {
     const selectedColumn = e.target.dataset.column;
     const selectedRow = e.target.dataset.row;
+    console.log("clicked");
+    const gameStatus = game.getGameStatus();
 
+    if (gameStatus) return; // Does not allow clicks after win.
     if (!selectedColumn) return;
     if (!selectedRow) return;
 
     game.playRound(selectedRow, selectedColumn);
     updateScreen();
   }
-  boardDiv.addEventListener("click", boardClickHandler);
+
+  // boardDiv.addEventListener("click", boardClickHandler);
 
   updateScreen();
 }
